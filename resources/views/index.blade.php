1dd73788f91a3619aa97dd8b9401f2ccca605fd7
Сейітмұрат Қалмұрат <seiitmratkalmrat@MacBook-Air-Seiitmurat.local>
2023-04-06 09:19:00 +0600

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,35 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-	<x-welcome-section/>
-	@if(!auth()->user() || !auth()->user()->test)
-		<x-index-hidden-first-section/>
-		<x-index-hidden-second-section/>
-	@else
-		<section>
-			<div class="mx-auto w-[1080px] py-[45px]">
-				<h3 class="mb-[40px] text-[26px] font-medium">Курсы которые вам подходят</h3>
-				<div class="mb-[22px] grid grid-cols-3 gap-[34px]">
-					@foreach($courses as $course)
-						<x-course-card :course="$course"/>
+	{{--	<x-welcome-section/>--}}
+	{{--	@if(!auth()->user() || !auth()->user()->test)--}}
+	{{--		<x-index-hidden-first-section/>--}}
+	{{--		<x-index-hidden-second-section/>--}}
+	{{--	@else--}}
+	<section>
+		<div class="mx-auto w-[1080px] py-[45px]">
+			<h3 class="mb-[40px] text-[26px] font-medium">Курсы которые вам подходят</h3>
+			<div class="mb-[22px] grid grid-cols-3 gap-[34px]">
+				@foreach($courses as $course)
+					<x-course-card :course="$course"/>
+				@endforeach
+			</div>
+		</div>
+	</section>
+	<section>
+		<div class="mx-auto w-[1080px] pb-[45px]">
+			<h3 class="mb-[30px] text-[26px] font-medium">Выберите эксперта</h3>
+			<div class="mb-[22px] grid grid-cols-4 gap-[54px]">
+				<div class="col-span-1">
+					<x-filter-experts/>
+				</div>
+				<div class="col-span-3 grid auto-rows-max grid-cols-3 gap-[28px]">
+					@foreach($experts as $expert)
+						<x-expert-card :expert="$expert"/>
 					@endforeach
 				</div>
 			</div>
-		</section>
-		<section>
-			<div class="mx-auto w-[1080px] pb-[45px]">
-				<h3 class="mb-[30px] text-[26px] font-medium">Выберите эксперта</h3>
-				<div class="mb-[22px] grid grid-cols-4 gap-[54px]">
-					<div class="col-span-1">
-						<x-filter-experts/>
-					</div>
-					<div class="col-span-3 grid auto-rows-max grid-cols-3 gap-[28px]">
-						@foreach($experts as $expert)
-							<x-expert-card :expert="$expert"/>
-						@endforeach
-					</div>
-				</div>
-			</div>
-		</section>
-	@endif
+		</div>
+	</section>
+	{{--	@endif--}}
 @endsection
