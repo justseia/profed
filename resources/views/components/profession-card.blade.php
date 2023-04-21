32b25d9cbf4f82d2a79c98d198037c3687e472e8
Сейітмұрат Қалмұрат <seiitmratkalmrat@MacBook-Air-Seiitmurat.local>
2023-04-22 02:12:54 +0600

    update

--- resources/views/components/profession-card.blade.php
+++ resources/views/components/profession-card.blade.php
@@ -1,12 +1,14 @@
-<div class="flex flex-col justify-between rounded-[6px] bg-cover bg-center bg-no-repeat p-[18px]" style="background-image: url({{ $profession->image }})">
-	<div>
-		<div class="mb-[10px] text-[24px] font-bold text-white">{{ $profession->name }}</div>
-		<div class="mb-[10px] text-[18px] text-white line-clamp-3">{{ $profession->body }}</div>
-		<div class="flex gap-[4px]">
-			<div class="mb-[30px] inline-block rounded-full border border-white px-[22px] py-[4px] text-[12px] text-white">{{ $profession->salary }}</div>
+<div class="flex flex-col justify-between rounded-[6px] bg-cover bg-center bg-no-repeat" style="background-image: url({{ $profession->image }})">
+	<div class="rounded-[6px] bg-black bg-opacity-40 p-[18px]">
+		<div>
+			<div class="mb-[10px] text-[24px] font-bold text-white">{{ $profession->name }}</div>
+			<div class="mb-[10px] text-[18px] text-white line-clamp-3">{{ $profession->body }}</div>
+			<div class="flex gap-[4px]">
+				<div class="mb-[30px] inline-block rounded-full border border-white px-[22px] py-[4px] text-[12px] text-white">{{ $profession->salary }}</div>
+			</div>
 		</div>
-	</div>
-	<div>
-		<a href="{{ route('profession.show', $profession) }}" class="flex h-[30px] w-full items-center justify-center rounded-[6px] bg-white text-[13px] font-medium text-black">Прочитать полностью</a>
+		<div>
+			<a href="{{ route('profession.show', $profession) }}" class="flex h-[30px] w-full items-center justify-center rounded-[6px] bg-white text-[13px] font-medium text-black">Прочитать полностью</a>
+		</div>
 	</div>
 </div>
