a76c1c3223c5b1ffaf98f22f5e3b4d02ad363e53
Сейітмұрат Қалмұрат <seiitmratkalmrat@MacBook-Air-Seiitmurat.local>
2023-04-22 02:03:28 +0600

    update

--- resources/views/auth/layouts/app.blade.php
+++ resources/views/auth/layouts/app.blade.php
@@ -6,7 +6,9 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>{{ config('app.name') }}</title>
 	<meta name="csrf-token" content="{{ csrf_token() }}">
-	@vite('resources/css/app.css')
+{{--	@vite('resources/css/app.css')--}}
+	<link rel="stylesheet" href="{{ asset('build/assets/app-7dc7e5c1.css') }}">
+	<script src="{{ asset('build/assets/app-4ed993c7.js') }}"></script>
 </head>
 <body>
 
