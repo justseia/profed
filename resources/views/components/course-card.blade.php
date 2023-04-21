2c957df859ed50c305158837e2be8b1d090af869
Сейітмұрат Қалмұрат <seiitmratkalmrat@MacBook-Air-Seiitmurat.local>
2023-04-22 02:16:17 +0600

    update

--- resources/views/components/course-card.blade.php
+++ resources/views/components/course-card.blade.php
@@ -1,6 +1,6 @@
 <div class="rounded-[6px] bg-cover bg-center bg-no-repeat" style="background-image: url({{ $course->image }})">
 	<div class="flex flex-col justify-between rounded-[6px] h-full bg-black bg-opacity-40 p-[18px]">
-		<div class="mb-[10px] text-[24px] overflow-auto font-bold text-white">{{ $course->name }}</div>
+		<div class="mb-[10px] text-[24px] overflow-auto font-bold text-white line-clamp-2">{{ $course->name }}</div>
 		<div class="flex gap-[4px]">
 			@foreach(json_decode($course->category) as $category)
 				<div class="mb-[30px] inline-block rounded-full border border-white px-[22px] py-[4px] text-[12px] text-white">{{ $category->name }}</div>
--- resources/views/components/profession-card.blade.php
+++ resources/views/components/profession-card.blade.php
@@ -1,7 +1,7 @@
 <div class="rounded-[6px] bg-cover bg-center bg-no-repeat" style="background-image: url({{ $profession->image }})">
 	<div class="flex flex-col justify-between rounded-[6px] h-full bg-black bg-opacity-40 p-[18px]">
 		<div>
-			<div class="mb-[10px] text-[24px] font-bold text-white">{{ $profession->name }}</div>
+			<div class="mb-[10px] text-[24px] font-bold text-white line-clamp-2">{{ $profession->name }}</div>
 			<div class="mb-[10px] text-[18px] text-white line-clamp-3">{{ $profession->body }}</div>
 			<div class="flex gap-[4px]">
 				<div class="mb-[30px] inline-block rounded-full border border-white px-[22px] py-[4px] text-[12px] text-white">{{ $profession->salary }}</div>
