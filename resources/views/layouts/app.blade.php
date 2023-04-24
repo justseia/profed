e2ed25fe9208072456b158faf0ce36c6e083bcab
Сейітмұрат Қалмұрат <seiitmratkalmrat@MacBook-Air-Seiitmurat.local>
2023-04-24 19:40:10 +0600

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+{{--<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">--}}
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>{{ config('app.name') }}</title>
 	<meta name="csrf-token" content="{{ csrf_token() }}">
-{{--	@vite('resources/css/app.css')--}}
-	<link rel="stylesheet" href="{{ asset('build/assets/app-554d38fe.css') }}">
-	<script src="{{ asset('build/assets/app-4ed993c7.js') }}"></script>
+	@vite('resources/css/app.css')
+{{--	<link rel="stylesheet" href="{{ asset('build/assets/app-554d38fe.css') }}">--}}
+{{--	<script src="{{ asset('build/assets/app-4ed993c7.js') }}"></script>--}}
 </head>
 <body>
 
